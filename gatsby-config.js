d2f6d4003369765253cf1266deb667b4e58fb75d
dw713 <116842920+dw713@users.noreply.github.com>
2022-10-30 23:56:26 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      Linkedin: `https://www.linkedin.com/in/dylanwallace000/`,
     },
   },
   plugins: [
